ccc4b3ae92f97c1c3424e8dbc7f2f09e62fa2415
Appas441 <appas.m@advertflair.studio>
2026-05-04 14:47:58 +0530

    debug: email logs

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -7,13 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 EMAIL = os.getenv("EMAIL")
-APP_PASSWORD = os.getenv("APP_PASSWORD")
-
+PASSWORD = os.getenv("APP_PASSWORD")
 
 def send_email(subject, content, to_email, cc_email=None):
     try:
         print("📧 EMAIL:", EMAIL)
-        print("🔑 PASSWORD EXISTS:", bool(APP_PASSWORD))
+        print("🔐 PASSWORD EXISTS:", bool(PASSWORD))
+        print("📨 TO:", to_email)
 
         msg = MIMEMultipart()
         msg["From"] = EMAIL
@@ -29,15 +29,18 @@ def send_email(subject, content, to_email, cc_email=None):
         if cc_email:
             recipients.append(cc_email)
 
+        print("🔌 Connecting Gmail SMTP...")
+
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
 
-        print("🔐 Logging in Gmail...")
-        server.login(EMAIL, APP_PASSWORD)
+        print("🔐 Logging in...")
+
+        server.login(EMAIL, PASSWORD)
 
         print("📤 Sending email...")
-        server.sendmail(EMAIL, recipients, msg.as_string())
 
+        server.sendmail(EMAIL, recipients, msg.as_string())
         server.quit()
 
         print("✅ Email sent successfully")
